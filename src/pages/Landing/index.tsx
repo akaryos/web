49cf2ecfd8c65541d7cb81b564651324d067f89a
Samuel Durante <hyossha@gmail.com>
2021-02-02 11:05:59 -0400

    Landing page

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Container } from './style'
+
+const Landing: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <h1>~Hello!</h1>
+        <p>We are closed for invitations, coming soon we will have a space for you!</p>
+      </Container>
+    </>
+  )
+}
+
+export default Landing
---
+++ src/pages/Landing/style.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+  
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+
+  h1 {
+    font-weight: 700;
+  }
+
+  p {
+    margin-top: 20px
+  }
+`
