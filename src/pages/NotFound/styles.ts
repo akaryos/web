709d60ffbcc329126e4e515bb36a91e707a2ecad
Samuel Durante <hyossha@gmail.com>
2021-02-25 23:21:33 -0400

    404 page

---
+++ src/pages/NotFound/styles.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  h1 {
+    font-size: 27px;
+    font-weight: 500;
+    border-right: 1px solid #C4C4C4;
+    margin-right: 20px;
+    padding-right: 20px;
+  }
+
+  h2 {
+    font-size: 16px;
+    font-weight: 400;
+  }
+`
